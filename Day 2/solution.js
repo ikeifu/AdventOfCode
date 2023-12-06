ab73df11a1b0d1903c002d15f2d0c5c892c708eb
Keith <fungsenkeith@gmail.com>
2023-12-06 15:31:04 -0500

    spliting down game data

--- Day 2/solution.js
+++ Day 2/solution.js
@@ -1,9 +1,53 @@
 const fs = require('node:fs')
 
+// const findMax = (data,r,g,b) => {
+// 	let gameText = ''
+// 	let i = 0
+// 	while(gameText !== ':'){
+// 		gameText = data[i]
+// 		i++ 
+// 	}
+// 	const slicedData = data.slice(i+1)
+// 	const checkIfFalse = (item, index, arr) => {
+// 		if(!isNaN(item))
+// 	}
+// 	slicedData.split(' ').forEach(checkIfFalse)
+//   return false
+// }
 fs.readFile('./data.txt', 'utf8', (_, data) => {
     const lines = data.split('\n')
-    lines.forEach(line => {
-      console.log(line)
+
+    let total = 0
+    // for(let i=0; i < lines.length; i++){
+    //   if(!findMax(lines[i],12,13,14)) total += i
+    // }
+	lines.forEach((line, i) => {
+		const [_, game] = line.split(': ')
+		const rounds = game.trim().split(';')
+    rounds.forEach(round => {
+      const turns = round.split(',')
+      turns.forEach(turn => {
+        const [num, color] = turn.trim().split(' ')
+        // console.log(num, color)
+        switch(color){
+          case 'red':
+            if(num > 12) {
+              total+=i
+              return
+            }
+          case 'blue':
+            if(num > 14) {
+              total+=i
+              return
+          }
+          case 'green':
+            if(num> 13) {
+              total+=i
+              return
+            }
+        }
+      })
     })
-  })
-console.log('pie')
+	})
+  console.log(total)
+})
